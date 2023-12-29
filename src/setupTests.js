e1598793e5aab4f8a29763bbf8e320eda1e136f4
sabitha-anasuri <154949365+sabitha-anasuri@users.noreply.github.com>
2023-12-29 10:13:50 -0800

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,18 @@
 // src/setupTests.js
-import { server } from './mocks/server';
 
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
+test('Drata users checking test case', async () => {
+  nock('https://api.drata.com')
+    .get('/users')
+    .reply(200, [
+      { id: 1, name: 'Sabitha' },
+      { id: 2, name: 'Erin' },
+    ]);
+
+  // Make API call using fetch or other HTTP client
+  const response = await fetch('https://api.drata.com/users');
+  // assert response data
+  expect(response.name).toBe('Sabitha Anasuri');
+  expect(response.age).toBeGreaterThan(18);
+  expect(Array.isArray(response.friends)).toBe(true);
+});
 
